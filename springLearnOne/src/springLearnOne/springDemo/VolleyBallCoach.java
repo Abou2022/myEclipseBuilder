52195770931caecc42716aa67ef842c59f4236b8
Bakary Sylla <syllabakary2002@gmail.com>
2022-12-04 08:48:29 -0800

    Implement VolleyBallCoach directly Process: new class implement add interface

---
+++ springLearnOne/src/springLearnOne/springDemo/VolleyBallCoach.java
@@ -0,0 +1,10 @@
+package springLearnOne.springDemo;
+
+public class VolleyBallCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice 4 times a day to become professional";
+	}
+
+}
